d7b7e5f883bce75418c747b1f4e2aff5aad2f28d
bharath <bharathaustin403@gmail.com>
2022-07-16 20:42:37 +0530

    button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { HomePageComponent } from './layout/home-page/home-page.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  { path: 'sidebar', component: SidebarComponent }
+  { path: 'sidebar', component: SidebarComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/layout/home-page/home-page.component.ts
+++ src/app/layout/home-page/home-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  @Output() public sendDta = new EventEmitter<number>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  btn() {
+    let counter = 0;
+    this.sendDta.emit((counter += 1));
   }
-
 }
